216d344dee15ef85aabd7a722b88fe0f9f67fe8f
Edu-Lu <moralesluis17106494@gmail.com>
2022-09-26 11:09:03 -0500

    Clase 047 Primer App - viewBinding

--- app/src/main/java/com/luismorales17106494/movieapp/MovieDetailFragment.kt
+++ app/src/main/java/com/luismorales17106494/movieapp/MovieDetailFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.luismorales17106494.movieapp.databinding.FragmentMovieDetailBinding
 
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+    private lateinit var binding: FragmentMovieDetailBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 }
