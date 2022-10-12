52f52322aff4422e15b41346b19fc300029ac596
Edu-Lu <moralesluis17106494@gmail.com>
2022-10-12 13:27:55 -0500

    Clase 054 Primer App - Cocluyendo nuestro DataSource

--- app/src/main/java/com/luismorales17106494/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/luismorales17106494/movieapp/data/remote/MovieDataSource.kt
@@ -1,16 +1,15 @@
 package com.luismorales17106494.movieapp.data.remote
 
+import com.luismorales17106494.movieapp.application.AppConstants
 import com.luismorales17106494.movieapp.data.model.MovieList
+import com.luismorales17106494.movieapp.repository.WebService
 
-class MovieDataSource {
+class MovieDataSource(private val webService: WebService) {
 
-    fun getUpComingMovies():MovieList{
-        return MovieList()
+    suspend fun getUpComingMovies(): MovieList {
+        return webService.getUpComingMovies(AppConstants.API_KEY)
     }
-    fun getTopRatedMovies():MovieList{
-        return MovieList()
-    }
-    fun getPopularMovies():MovieList{
-        return MovieList()
-    }
+
+    suspend fun getTopRatedMovies(): MovieList = webService.getTopRatedMovies(AppConstants.API_KEY)
+    suspend fun getPopularMovies(): MovieList = webService.getPopularMovies(AppConstants.API_KEY)
 }
